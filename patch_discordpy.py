64bfa1ff7a8ba88320b6dcc263755d45a3090e90
INFERNOX.DEV SOFTWARE <dev@inx1.ru>
2024-06-13 17:12:14 +0300

    Modify

--- patch_discordpy.py
+++ patch_discordpy.py
@@ -5,40 +5,42 @@ from sys import platform
 def patch_discordpy() -> None:
     async def identify(self):
         payload = {
-            'op': self.IDENTIFY,
-            'd': {
-                'token': self.token,
-                'properties': {
-                    '$os': platform,
-                    '$browser': 'Discord Android',
-                    '$device': 'Discord Android',
-                    '$referrer': '',
-                    '$referring_domain': ''
+            "op": self.IDENTIFY,
+            "d": {
+                "token": self.token,
+                "properties": {
+                    "$os": platform,
+                    "$browser": "Discord Android",
+                    "$device": "Discord Android",
+                    "$referrer": "",
+                    "$referring_domain": "",
                 },
-                'compress': True,
-                'large_threshold': 250,
-                'v': 3
-            }
+                "compress": True,
+                "large_threshold": 250,
+                "v": 3,
+            },
         }
 
         if self.shard_id is not None and self.shard_count is not None:
-            payload['d']['shard'] = [self.shard_id, self.shard_count]
+            payload["d"]["shard"] = [self.shard_id, self.shard_count]
 
         state = self._connection
 
         if state._activity is not None or state._status is not None:
-            payload['d']['presence'] = {
-                'status': state._status,
-                'game': state._activity,
-                'since': 0,
-                'afk': False
+            payload["d"]["presence"] = {
+                "status": state._status,
+                "game": state._activity,
+                "since": 0,
+                "afk": False,
             }
 
         if state._intents is not None:
-            payload['d']['intents'] = state._intents.value
+            payload["d"]["intents"] = state._intents.value
 
-        await self.call_hooks('before_identify', self.shard_id, initial=self._initial_identify)
+        await self.call_hooks(
+            "before_identify", self.shard_id, initial=self._initial_identify
+        )
         await self.send_as_json(payload)
-        gateway._log.info('Shard ID %s has sent the IDENTIFY payload.', self.shard_id)
+        gateway._log.info("Shard ID %s has sent the IDENTIFY payload.", self.shard_id)
 
     gateway.DiscordWebSocket.identify = identify
